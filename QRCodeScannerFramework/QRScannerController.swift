9bb637b171098f1bb5d222bd8b459ad314605e0c
Hussein Kishk <hussein.kishk@code95.info>
2018-03-26 16:54:53 +0200

    initial commit

---
+++ QRCodeScannerFramework/QRScannerController.swift
@@ -0,0 +1,91 @@
+//
+//  QRScannerController.swift
+//  QRCodeReader
+//
+//  Created by Hussein on 13/3/2018.
+//  Copyright © 2018 Code95. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+public protocol QrScannerDelegate {
+    func getScannedData(data: String)
+}
+
+public class QRScannerController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    
+    public var qrCodeFrameView:UIView?
+    let supportedCodeTypes = [AVMetadataObject.ObjectType.qr]
+    public var qrScannerDelegate: QrScannerDelegate!
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {return}
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = supportedCodeTypes
+            
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            captureSession?.startRunning()
+            
+            qrCodeFrameView = UIView()
+            
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                self.view.addSubview(qrCodeFrameView)
+                self.view.bringSubview(toFront: qrCodeFrameView)
+            }
+            
+            captureSession?.startRunning()
+            
+        } catch {
+            // If any error occurs, simply print it out and don't continue any more.
+            print(error)
+            return
+        }
+    }
+    
+    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            self.navigationController?.popViewController(animated: true)
+            qrScannerDelegate.getScannedData(data: "No QR code is detected")
+            captureSession?.stopRunning()
+            return
+        }
+
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+
+            if metadataObj.stringValue != nil {
+                self.navigationController?.popViewController(animated: true)
+                qrScannerDelegate.getScannedData(data: (metadataObj.stringValue)!)
+                captureSession?.stopRunning()
+            }
+        }
+    }
+
+}
